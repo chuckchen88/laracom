63c06ad918ce2bbfd24d7a2db958e74e95bdc30e
chenlong <chenlong@dalongyun.com>
2021-01-27 16:48:26 +0800

    新增用户更新和密码重置相关服务接口

--- user-service/main.go
+++ user-service/main.go
@@ -12,7 +12,6 @@ import (
 )
 
 func main() {
-
 	// 创建数据库连接，程序退出时断开连接
 	db, err := database.CreateConnection()
 	defer db.Close()
@@ -24,9 +23,11 @@ func main() {
 	// 和 Laravel 数据库迁移类似
 	// 每次启动服务时都会检查，如果数据表不存在则创建，已存在检查是否有修改
 	db.AutoMigrate(&pb.User{})
+	db.AutoMigrate(&pb.PasswordReset{})
 
 	// 初始化 Repo 实例用于后续数据库操作
 	repo := &repository.UserRepository{db}
+	resetRepo := &repository.PasswordResetRepository{db}
 	// 初始化 token service
 	token := &service.TokenService{repo}
 
@@ -38,7 +39,7 @@ func main() {
 	srv.Init()
 
 	// 注册处理器
-	pb.RegisterUserServiceHandler(srv.Server(), &handler.UserService{repo, token})
+	pb.RegisterUserServiceHandler(srv.Server(), &handler.UserService{repo, resetRepo,token})
 
 	// 启动用户服务
 	if err := srv.Run(); err != nil {
--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -3,9 +3,20 @@ package laracom_service_user
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/satori/go.uuid"
+	"time"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
 	uuid := uuid.NewV4()
 	return scope.SetColumn("Id", uuid.String())
+}
+
+func (model *User) BeforeSave(scope *gorm.Scope) error {
+	_ = scope.SetColumn("UpdatedAt", time.Now().Format(time.RFC3339))
+	return nil
+}
+
+func (model *PasswordReset) BeforeCreate(scope *gorm.Scope) error {
+	_ = scope.SetColumn("CreatedAt", time.Now().Format(time.RFC3339))
+	return nil
 }
